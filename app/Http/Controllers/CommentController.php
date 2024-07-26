9b1552906c5d76b4452bd7b8a32e1fefe4c075b8
yuutakun-2 <134118387+yuutakun-2@users.noreply.github.com>
2024-07-27 00:25:53 +0630

    Update CommentController.php for emailing subscribers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SubscriberMail;
 use App\Models\Comment;
 use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -14,10 +17,21 @@ class CommentController extends Controller
             'body' => 'required'
         ]);
 
-    $blog->comments()->create([
+    $comment = $blog->comments()->create([
         'user_id' => auth()->id(),
         'body' => request('body')
     ]);
+
+    //add email to subscribers function when a comment is added to a post
+    //filter subscribed users except currently logged in user
+    $subscribers = $blog->users->filter(function ($user) {
+        return $user->id !== auth()->id();
+    });
+
+    foreach ($subscribers as $subscriber) {
+        Mail::to($subscriber->email)->queue(new SubscriberMail($subscriber, $comment));
+    }
+
 //         $comment = new Comment();
 //         $comment->blog_id = $blog->id;
 //         $comment->user_id = auth()->id();
@@ -25,4 +39,5 @@ class CommentController extends Controller
 //         $comment->save();
     return back();
     }   
+    
 }
