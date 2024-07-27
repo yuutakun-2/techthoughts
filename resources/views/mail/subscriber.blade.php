06d3c23fe84499ddae5f71eda0afc089bffebd86
Yuuta <arkarchanmyae2@gmail.com>
2024-07-27 17:56:04 +0630

    Added emailing subscribers

---
+++ resources/views/mail/subscriber.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome, {{$subscriber->name}}</h1>
+    <h1>Hehe</h1>
+    <h1>Body, {{$comment->body}}</h1>
+</body>
+</html>
