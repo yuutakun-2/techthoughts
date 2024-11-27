ce0deeb0d3b74b1da8fa41a976f18a52ce652aaa
Yuuta <arkarchanmyae2@gmail.com>
2024-11-27 14:35:45 +0630

    next() works, prev() needs to fix

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,18 +1,58 @@
 let box = 1;
+// let boxLeft = box - 1;
+// if (boxLeft < 1) boxLeft = 3;
+// let boxRight = box + 1;
+// if (boxRight > 3) boxRight = 1;
 
 function next() {
-    document.querySelector(`#box${box}`).style.display = "none";
+    //box1 to 2
+    //box1 logic
+    document.querySelector(`#box${box}`).classList.remove("active");
+    document.querySelector(`#box${box}`).classList.add("back");
+    //above box becomes boxLeft
+    //box2 logic
     box++;
-    if (box > 3) box = 1;
-    document.querySelector(`#box${box}`).style.display = "block";
+    if (box > 3) {
+        box = 1;
+        // document.querySelector(`#box${box}`).classList.add("active");
+    }
+    document.querySelector(`#box${box}`).classList.add("active");
+    //above box becomes current box
+    //box3 logic
+    let boxRight = box + 1;
+    if (boxRight > 3) boxRight = 1;
+    document.querySelector(`#box${boxRight}`).classList.remove("back");
+    //above box becomes boxRight
 }
 function prev() {
-    document.querySelector(`#box${box}`).style.display = "none";
+    //box3 to 2
+    //box3 logic
+    document.querySelector(`#box${box}`).classList.remove("active");
+    //box1
     box--;
-    if (box < 1) box = 1;
-    document.querySelector(`#box${box}`).style.display = "block";
+    if (box < 1) {
+        box = 3;
+        document.querySelector(`#box${box}`).classList.remove("back");
+        document.querySelector(`#box${box}`).classList.add("active");
+    }
+    document.querySelector(`#box${box}`).classList.remove("back");
+    document.querySelector(`#box${box}`).classList.add("active");
+    //box2 logic
+    // box--;
+    // if (box < 1) {
+    //     box = 3;
+    // }
+    let boxRight = box + 1;
+    if (boxRight > 3) {
+        boxRight = 1;
+        document.querySelector(`#box${boxRight}`).classList.remove("active");
+        document.querySelector(`#box${boxRight}`).classList.add("back");
+    }
+    document.querySelector(`#box${boxRight}`).classList.remove("active");
+    document.querySelector(`#box${boxRight}`).classList.add("back");
 }
+
 document.querySelector("#next").onclick = next;
 document.querySelector("#prev").onclick = prev;
 
-setInterval(next, 4000);
+// setInterval(next, 4000);
--- resources/views/blogs/home.blade.php
+++ resources/views/blogs/home.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
     <section class="w-full max-h-[60vh] mx-auto items-center">
-        <div class="carousel-box bg-gray-500 relative">
-            <div class="box" id="box1">
-                <div class="box-image" style="background: url('img/3.png') no-repeat center/cover;"></div>
+        <div class="carousel-box bg-gray-500 ">
+            <div class="box active" id="box1">
+                <div class="box-image" style="background: url('img/3.png') no-repeat center/cover;"><span>1</span></div>
             </div>
             <div class="box" id="box2">
-                <div class="box-image" style="background: url('img/4.png') no-repeat center/cover;"></div>
+                <div class="box-image" style="background: url('img/4.png') no-repeat center/cover;"><span>2</span></div>
             </div>
             <div class="box" id="box3">
-                <div class="box-image" style="background: url('img/5.png') no-repeat center/cover;"></div>
+                <div class="box-image" style="background: url('img/5.png') no-repeat center/cover;"><span>3</span></div>
             </div>
-            <div class="controls absolute w-full top-1/2 flex justify-between items-center">
+            <div class="controls absolute w-full h-full top-1/2 flex justify-between items-center">
                 <button id="prev">&laquo; Prev</button>
                 <button id="next">Next &raquo;</button>
             </div>
